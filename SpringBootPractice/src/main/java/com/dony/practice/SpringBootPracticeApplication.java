fd3e06dcbd0034b923e63aabc1f68d2ba56756ca
Donydavid <davidprb96@gmail.com>
2022-04-01 22:57:30 +0530

    1st push

---
+++ SpringBootPractice/src/main/java/com/dony/practice/SpringBootPracticeApplication.java
@@ -0,0 +1,16 @@
+package com.dony.practice;
+
+import org.slf4j.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeApplication {
+	public static final Logger logger = LoggerFactory.getLogger(SpringBootPracticeApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("started");
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+
+}
