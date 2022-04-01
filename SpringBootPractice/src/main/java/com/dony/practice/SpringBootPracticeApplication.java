0d7defe09ff26634d4011540e59bcd004f473124
Donydavid <davidprb96@gmail.com>
2022-04-01 23:10:51 +0530

    pushing

--- SpringBootPractice/src/main/java/com/dony/practice/SpringBootPracticeApplication.java
+++ SpringBootPractice/src/main/java/com/dony/practice/SpringBootPracticeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootPracticeApplication {
-	public static final Logger logger = LoggerFactory.getLogger(SpringBootPracticeApplication.class);
+	public static final Logger logger =  LoggerFactory.getLogger(SpringBootPracticeApplication.class);
 
 	public static void main(String[] args) {
 		logger.info("started");
