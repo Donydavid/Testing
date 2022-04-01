2562e91dd860319695d9fc53b94b68a0e395de45
Donydavid <davidprb96@gmail.com>
2022-04-01 23:30:51 +0530

    pushing with comments of loggger

--- SpringBootPractice/src/main/java/com/dony/practice/SpringBootPracticeApplication.java
+++ SpringBootPractice/src/main/java/com/dony/practice/SpringBootPracticeApplication.java
@@ -1,16 +1,24 @@
 package com.dony.practice;
 
 import org.slf4j.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPracticeApplication {
+public class SpringBootPracticeApplication implements CommandLineRunner {
 	public static final Logger logger =  LoggerFactory.getLogger(SpringBootPracticeApplication.class);
 
 	public static void main(String[] args) {
 		logger.info("started");
 		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("from command line arguments");
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- SpringBootPractice/src/test/java/com/dony/practice/SpringBootPracticeApplicationTests.java
+++ SpringBootPractice/src/test/java/com/dony/practice/SpringBootPracticeApplicationTests.java
@@ -1,13 +1,19 @@
 package com.dony.practice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootPracticeApplicationTests {
-
+	public static final Logger logger =  LoggerFactory.getLogger(SpringBootPracticeApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("from test class");
+		assertEquals(true, true);
 	}
 
 }
